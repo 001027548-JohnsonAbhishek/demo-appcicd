2a277194035267bf79fa9c2865d9302d6ec61995
JonAbhi <njabhishek96@gmail.com>
2021-12-08 20:58:05 -0800

    demo

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,7 +2,9 @@
 sleep 5m
 cd /home/ubuntu/webapp/webapp
 sudo su
+touch application.log
+sudo chmod 777 application.log
 sudo mvn clean
 sudo mvn clean install
-(sudo java -jar target/*.jar . &)
+(sudo java -jar target/*.jar . &) > application.log 2>&1
 sleep 1m
